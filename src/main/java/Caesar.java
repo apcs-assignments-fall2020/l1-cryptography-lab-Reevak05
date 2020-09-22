b97565cda525f9867dadf99f0168ee314ba0d871
Reevak05 <kaveergera@gmail.com>
2020-09-22 17:15:27 +0000

    Create Caesar cipher programs

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -2,23 +2,45 @@ import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String converted = "";
+        for (int i = 0; i<message.length();i++) {
+            if (((message.charAt(i)>=65) && (message.charAt(i)<=90-3))||((message.charAt(i)>=97) && (message.charAt(i)<=122-3))) converted += (char)(message.charAt(i)+3);
+            else if (((message.charAt(i)>90-3) && (message.charAt(i)<=90))||((message.charAt(i)>122-3) && (message.charAt(i)<=122))) converted += (char)(message.charAt(i)-(26-3));
+            else converted += (char)(message.charAt(i));
+        }
+        return converted;
     }
 
     public static String decryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String converted = "";
+        for (int i = 0; i<message.length();i++) {
+            if (((message.charAt(i)>=65+3) && (message.charAt(i)<=90))||((message.charAt(i)>=97+3) && (message.charAt(i)<=122))) converted += (char)(message.charAt(i)-3);
+            else if (((message.charAt(i)>=65) && (message.charAt(i)<=90))||((message.charAt(i)>=97) && (message.charAt(i)<=122))) converted += (char)(message.charAt(i)+(26-3));
+            else converted += (char)(message.charAt(i));
+        }
+        return converted;
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        if (key>26) key = key%26;
+        String converted = "";
+        for (int i = 0; i<message.length();i++) {
+            if (((message.charAt(i)>=65) && (message.charAt(i)<=90-key))||((message.charAt(i)>=97) && (message.charAt(i)<=122-key))) converted += (char)(message.charAt(i)+key);
+            else if (((message.charAt(i)>90-key) && (message.charAt(i)<=90))||((message.charAt(i)>122-key) && (message.charAt(i)<=122))) converted += (char)(message.charAt(i)-(26-key));
+            else converted += (char)(message.charAt(i));
+        }
+        return converted;
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        if (key>26) key = key%26;
+        String converted = "";
+         for (int i = 0; i<message.length();i++) {
+            if (((message.charAt(i)>=65+key) && (message.charAt(i)<=90))||((message.charAt(i)>=97+key) && (message.charAt(i)<=122))) converted += (char)(message.charAt(i)-key);
+            else if (((message.charAt(i)>=65) && (message.charAt(i)<=90))||((message.charAt(i)>=97) && (message.charAt(i)<=122))) converted += (char)(message.charAt(i)+(26-key));
+            else converted += (char)(message.charAt(i));
+        }
+        return converted;
     }
 
 
